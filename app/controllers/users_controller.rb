6b5771ef3a2de4e5932719e9ff7652ca2670dfac
camille-arce <117893473+camille-arce@users.noreply.github.com>
2023-04-16 23:28:26 +0000

    new changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def index
-    
     @list_of_users = User.all.order({ :username => :asc })
 
     render({ :template => "user_templates/index.html.erb" })
+  end
+
+  def show
+    @username = params.fetch("username_path")
+
+    render({ :template => "user_templates/show.html.erb" })
   end
 end
